cb22bb46a01e2a47520daf3d827f30c55086afb2
mmint.m <pheeraya.p@ku.th>
2021-02-18 16:36:03 +0700

    ข้อ2 loop2

--- 2.c
+++ 2.c
@@ -2,10 +2,20 @@
 int main()
 {
    int i,j,k,p,a,m=0,t[48]={},prime_t;
-   scanf("%d",&n);
+   scanf("%d",&j);
    for(i=0;i<j;i++)
    {
       scanf("%d %d",&p,&a);
       t[k-1] = t[k-1]+a;
    }
+   for(i=0;i<48;i++)
+   {
+      if(m<t[i])
+      {
+      m = t[i];
+      prime_t = i;
+      }
+   }
+   printf("%d %d",prime_t+1,t[prime_t]);
 }
+
