fc11f3b74f6874ffd3d09a16873f67288b372982
mmint.m <pheeraya.p@ku.th>
2021-02-18 17:02:20 +0700

    ได้ลูปใน

--- 3.c
+++ 3.c
@@ -1,1 +1,19 @@
+#include<stdio.h>
+int main ()
+{
+   int k,i=2,j=0;
+   scanf("%d",&k);
+   if(k==1)
+   {
+      printf("1\n");
+   }
+   while(k!=1)
+   {
+      while(k%i==0)
+   {
+      printf("%d\n",i);
+      k /= i;
+   }
 
+}
+}
