0ac0289c0567c90a780bfcc36259df99ddbe1310
mmint.m <pheeraya.p@ku.th>
2021-02-18 16:23:38 +0700

    forแรกข้อ2

---
+++ 2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+   int i,j,k,p,a,m=0,t[48]={},prime_t;
+   scanf("%d",&n);
+   for(i=0;i<j;i++)
+   {
+      scanf("%d %d",&p,&a);
+      t[k-1] = t[k-1]+a;
+   }
+}
